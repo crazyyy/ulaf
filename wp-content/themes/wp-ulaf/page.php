838131188d3ffcf807949ed0301acfb180b00f98
crazyyy <crazyyy@gmail.com>
2016-04-19 21:18:48 +0300

    wordpress start

--- wp-content/themes/wp-ulaf/page.php
+++ wp-content/themes/wp-ulaf/page.php
@@ -1,18 +1,31 @@
 <?php get_header(); ?>
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-      <h1 class="page-title inner-title"><?php the_title(); ?>1231232</h1>
-      <?php the_content(); ?>
-      <?php edit_post_link(); ?>
+  <div class="section history">
+   <div class="container">
+      <div class="row">
 
-    </article>
-  <?php endwhile; else: // If 404 page error ?>
-    <article>
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+          <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-12'); ?>>
 
-      <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
+            <h1 class="page-title inner-title"><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+            <?php edit_post_link(); ?>
 
-    </article>
-  <?php endif; ?>
-<?php get_sidebar(); ?>
+          </article>
+        <?php endwhile; else: // If 404 page error ?>
+          <article class="col-md-12">
+
+            <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
+
+          </article>
+        <?php endif; ?>
+
+      </div>
+    </div>
+  </div>
+
+  <?php get_template_part('relative-pages'); ?>
+
+
+
 <?php get_footer(); ?>
