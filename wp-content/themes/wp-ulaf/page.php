1dc5a0d9e5d68a7d856cf7614a2e8f840b48349e
Anton <forvinwolves@gmail.com>
2016-04-20 14:55:21 +0300

    wordpress

--- wp-content/themes/wp-ulaf/page.php
+++ wp-content/themes/wp-ulaf/page.php
@@ -1,18 +1,49 @@
 <?php get_header(); ?>
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <div class="section history">
+     <div class="container">
+        <div class="row">
 
-      <h1 class="page-title inner-title"><?php the_title(); ?>1231232</h1>
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(col-md-12); ?>>
+
+<h2><?php the_title(); ?></h2>
+<?php the_content(); ?>
+
+<h1 class="page-title inner-title"><?php the_title(); ?></h1>
       <?php the_content(); ?>
       <?php edit_post_link(); ?>
 
     </article>
   <?php endwhile; else: // If 404 page error ?>
-    <article>
+    <article class="col-md-12">
 
       <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
 
     </article>
   <?php endif; ?>
-<?php get_sidebar(); ?>
+
+
+          <div class="">
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+                    <div class="section photo">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/img/teams/sbor.jpeg">
+                                </div>
+                                <div class="col-md-4">
+                                  <img src="<?php echo get_template_directory_uri(); ?>/img/teams/sbor2.jpeg">
+                                </div>
+                                <div class="col-md-4"></div>
+                            </div>
+                        </div>
+                    </div>
+
 <?php get_footer(); ?>
