3c526d857f54fecc2baa888e6b5b86dd4f0e44b8
crazyyy <crazyyy@gmail.com>
2016-04-21 16:05:37 +0300

    wordpress updated

--- wordpress/wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wordpress/wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf4a76b54f454685d869d012b4e7f9b27::getLoader();
+return ComposerAutoloaderInit8f412f0bce78c2c2ae84a1163b773a57::getLoader();
