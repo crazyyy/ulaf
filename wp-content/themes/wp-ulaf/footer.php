2f29ea76879abe35d823ee3792841d8e13030a1b
crazyyy <crazyyy@gmail.com>
2016-04-21 13:47:02 +0300

    category ready

--- wp-content/themes/wp-ulaf/footer.php
+++ wp-content/themes/wp-ulaf/footer.php
@@ -1,53 +1,37 @@
-<<<<<<< HEAD
- <footer>
-=======
     <footer>
->>>>>>> master
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <p>© Ulaf 2016</p>
-                </div>
-                <div class="col-sm-6">
+      <div class="container">
+        <div class="row">
 
-                            <ul class="list-inline social">
-                                <li>
-                                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/email.png" alt="ulaf"></a>
-                                </li>
-                                <li>
-                                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/facebook.png" alt="ulaf Facebook"></a>
-                                </li>
-                                </li>
-                                <li>
-                                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/vkontakte.png" alt="ulaf VK"></a>
-                                </li>
-                                <li>
-                                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/twitter.png" alt="ulaf Twitter"></a>
-                                </li>
-                                <li>
-                                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/youtube.png" alt="ulaf YouTube"></a>
-                                </li>
-                                <li>
-                                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/instagram.png" alt="ulaf Instagram"></a>
-                                </li>
-                            </ul>
+          <div class="col-sm-6">
+            <p>© Ulaf 2016</p>
+          </div><!-- col-sm-6 -->
 
-                </div>
-<<<<<<< HEAD
+          <div class="col-sm-6">
+            <ul class="list-inline social">
+              <li>
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/email.png" alt="ulaf"></a>
+              </li>
+              <li>
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/facebook.png" alt="ulaf Facebook"></a>
+              </li>
+              </li>
+              <li>
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/vkontakte.png" alt="ulaf VK"></a>
+              </li>
+              <li>
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/twitter.png" alt="ulaf Twitter"></a>
+              </li>
+              <li>
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/youtube.png" alt="ulaf YouTube"></a>
+              </li>
+              <li>
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/social/instagram.png" alt="ulaf Instagram"></a>
+              </li>
+            </ul>
+          </div><!-- col-sm-6 -->
 
-            </div>
-        </div>
+        </div><!-- row -->
+      </div><!-- container -->
     </footer>
-
-
-
-=======
-
-            </div>
-        </div>
-    </footer>
->>>>>>> master
-
-</body>
-
+  </body>
 </html>
--- wp-content/themes/wp-ulaf/loop.php
+++ wp-content/themes/wp-ulaf/loop.php
@@ -1,29 +1,35 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-  <div id="post-<?php the_ID(); ?>" <?php post_class('looper'); ?>>
 
-    <a rel="nofollow" class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-      <?php if ( has_post_thumbnail()) :
-        the_post_thumbnail('medium');
-      else: ?>
-        <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
-      <?php endif; ?>
-    </a><!-- /post thumbnail -->
+  <div class="row">
+    <div class="col-md-4">
+      <a rel="nofollow" class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+        <?php if ( has_post_thumbnail()) :
+          the_post_thumbnail('medium');
+        else: ?>
+          <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+        <?php endif; ?>
+      </a><!-- /post thumbnail -->
+    </div>
 
-    <h2 class="inner-title">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-    </h2><!-- /post title -->
+    <div class="col-md-8">
+      <h2 class="inner-title">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+      </h2><!-- /post title -->
+      <p class="date"><?php the_time('j.m.Y'); ?></p>
 
-    <span class="date"><?php the_time('j F Y'); ?> <span><?php the_time('G:i'); ?></span></span>
-    <span class="author"><?php _e( 'Published by', 'wpeasy' ); ?> <?php the_author_posts_link(); ?></span>
-    <span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'wpeasy' ), __( '1 Comment', 'wpeasy' ), __( '% Comments', 'wpeasy' )); ?></span><!-- /post details -->
+      <?php wpeExcerpt('wpeExcerpt20'); ?>
 
-    <?php wpeExcerpt('wpeExcerpt40'); ?>
+      <a href="<?php the_permalink(); ?>" class="read-more">Читать полностью...</a>
+    </div>
 
-  </div><!-- /looper -->
+  </div><!-- /.row -->
+
   <?php endwhile; else: ?>
-  <div>
 
-    <h2 class="title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
+      </div><!-- col-md-12 -->
+    </div><!-- /.row -->
 
-  </div><!-- /article -->
 <?php endif; ?>
