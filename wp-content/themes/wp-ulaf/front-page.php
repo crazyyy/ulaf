0aea6718a2695c7fc8d041eeb35f5dbb766479ca
Anton <forvinwolves@gmail.com>
2016-04-26 11:51:50 +0300

    front-page-slider

--- wp-content/themes/wp-ulaf/front-page.php
+++ wp-content/themes/wp-ulaf/front-page.php
@@ -1,21 +1,50 @@
-<?php get_header(); ?>
+<?php /* Template Name: Home Page */ get_header(); ?>
 
    <!-- slider -->
 
-    <div id="fullcarousel-example" data-interval="6000" class="carousel slide" data-ride="carousel">
+    <div id="fullcarousel-example" data-interval="600" class="carousel slide" data-ride="carousel">
+
         <div class="carousel-inner">
-            <div class="item active">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/slider1.jpg">
+
+
+
+                <?php
+
+          $images = get_field('slider_images');
+
+          if( $images ):
+$i = 0;
+
+
+
+
+
+ foreach( $images as $image ):
+
+if ( $i == 0) {
+  $active = "active";
+} else {
+  $active = "";
+}
+
+  ?>
+
+
+            <div class="item <?php echo $active; ?>">
+
+             <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
             </div>
-            <div class="item">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/slider2.jpg">
-            </div>
-            <div class="item">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/slider3.jpg">
-            </div>
-            <div class="item">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/slider4.jpg">
-            </div>
+
+<?php $i++;  ?>
+
+            <?php endforeach; ?>
+            <?php endif; ?>
+
+
+
+
+
+
         </div>
     </div>
     <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"><i class="icon-prev fa fa-angle-left"></i></a>
@@ -24,11 +53,11 @@
     <div class="item">
         <div class="carousel-caption">
             <div class="slider_title">
-                <h1>Новость, которую ждали все игроки и любители - Американского футбола в Украине!</h1>
+                <h1><?php the_field('slide_title');?></h1>
             </div>
 
             <div class="descr">
-                <p>Друзья! Новость, которую ждали все игроки и любители Американского футбола в Украине! ...</p>
+                <p><?php the_field('slide_description');?></p>
             </div>
             <div class="slider_but">
                 <a class="btn btn-primary">Присоединяйтесь</a>
@@ -48,34 +77,25 @@
           <h3>Последние Новости</h3>
 
           <!-- start news loop -->
-          <div class="col-md-3">
-            <a href="#" class="hover_image">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/content/news1.jpg">
-              <span class="date">19.04.2016 16:25</span>
+
+         <?php query_posts("showposts=4&cat=1"); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+
+<div class="col-md-3">
+            <a href="<?php the_permalink(); ?>" class="hover_image">
+              <?php if ( has_post_thumbnail()) :
+          the_post_thumbnail('medium');
+        else: ?>
+          <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+        <?php endif; ?>
+              <span class="date"><?php the_time('j.m.Y'); ?> <?php the_time('H:i'); ?></span>
             </a>
-            <h4><a href="#">Американский футбол в интересных фактах и цифрах</a></h4>
+            <h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h4>
           </div>
-          <div class="col-md-3">
-            <a href="#" class="hover_image">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/content/news2.jpg">
-              <span class="date">19.04.2016 16:25</span>
-            </a>
-            <h4><a href="#">Календаль-Сезон 2016 Ulaf Дивизион C</a></h4>
-          </div>
-          <div class="col-md-3">
-            <a href="#" class="hover_image">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/content/news3.jpg">
-              <span class="date">19.04.2016 16:25</span>
-            </a>
-            <h4><a href="#">Наш Друг и партнер</a></h4>
-          </div>
-          <div class="col-md-3">
-            <a href="#" class="hover_image">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/content/news4.jpg">
-              <span class="date">19.04.2016 16:25</span>
-            </a>
-            <h4><a href="#">Календаль-Сезон 2016 Ulaf Дивизион B</a></h4>
-          </div>
+
+<?php endwhile; endif; ?>
+<?php wp_reset_query(); ?>
           <!-- end news loop -->
         </div><!-- news_block -->
 
