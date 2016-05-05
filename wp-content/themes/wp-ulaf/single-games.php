557c497918690829a6f9b0ab0544c8ec674b990d
Anton <forvinwolves@gmail.com>
2016-05-05 12:46:31 +0300

    team-people

--- wp-content/themes/wp-ulaf/single-games.php
+++ wp-content/themes/wp-ulaf/single-games.php
@@ -16,37 +16,32 @@
                 <td>
 
                   <?php
-
-$posts = get_field('team');
-
-if( $posts ): ?>
-    <ul>
-    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
-        <?php setup_postdata($post); ?>
-        <li>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </li>
-    <?php endforeach; ?>
-    </ul>
-    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-<?php endif; ?>
-                </td>
-
-          <td>
-
-                <?php
-                      $posts = get_sub_field('opposing_team');
+                      $posts = get_field('team');
                       if( $posts ):
                         foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
                 <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $p->ID ) ); ?>" alt="">
 
                         <?php endforeach; ?>
-                      <?php endif; ?>
+
+                    <?php endif; ?>
+                </td>
+
+          <td>
+
+                <?php
+                      $posts = get_field('opposing_team');
+                      if( $posts ):
+                        foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
+                <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $p->ID ) ); ?>" alt="">
+
+                        <?php endforeach; ?>
+
+                    <?php endif; ?>
 
           </td>
 
 
-                     <?php endwhile; endif; ?>
+
 
           <td><?php the_field('score');?></td>
 
