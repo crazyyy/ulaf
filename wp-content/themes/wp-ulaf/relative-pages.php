f573987a80843f919293caa312e5c1cb638babf3
Anton <forvinwolves@gmail.com>
2016-05-05 16:55:23 +0300

    Relative-pages

--- wp-content/themes/wp-ulaf/relative-pages.php
+++ wp-content/themes/wp-ulaf/relative-pages.php
@@ -3,14 +3,15 @@
     <div class="row">
 
       <div class="col-md-4">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/teams/sbor.jpeg">
+
+      <?php wpb_related_pages(); ?>
+      <?php if ( has_post_thumbnail()) :?>
+
+                 <?php the_post_thumbnail('full'); // Fullsize image for the single post ?>
+
+            <?php endif; ?><!-- /post thumbnail -->
       </div>
-      <div class="col-md-4">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/teams/sbor2.jpeg">
-      </div>
-      <div class="col-md-4">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/teams/sbor2.jpeg">
-      </div>
+
 
     </div><!-- row -->
   </div><!-- container -->
