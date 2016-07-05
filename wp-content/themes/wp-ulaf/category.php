ea40c0189d7069070b84fccc3e546e129b332c18
Anton <forvinwolves@gmail.com>
2016-07-05 19:15:30 +0300

    category news Task -14

--- wp-content/themes/wp-ulaf/category.php
+++ wp-content/themes/wp-ulaf/category.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
-
   <section class="section-news category">
     <div class="container">
       <div class="row category-news">
+
 
         <h1 class="cat-title inner-title col-md-12"><?php _e( 'Categories for', 'wpeasy' ); the_category(', '); ?></h1>
 
