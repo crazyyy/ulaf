8a415a31e72d75b49faf44aad424d26ed9a9f207
crazyyy <crazyyy@gmail.com>
2016-07-31 14:13:48 +0300

    before update ACF to pro

--- wp-content/themes/wp-ulaf/page-gallery.php
+++ wp-content/themes/wp-ulaf/page-gallery.php
@@ -1,33 +1,30 @@
-<?php /* Template Name: Gallery page */ get_header(); ?>
+<?php /* Template Name: Gallery Page */ get_header(); ?>
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<div class="section content">
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <div class="section content">
       <div class="container">
         <div class="row">
 
-        <!-- Gallery Page -->
-          <div class="col-md-12 gallery">
+          <!-- Gallery Page -->
+            <div class="col-md-12 gallery">
               <?php $posts = get_field('gallery'); if( $posts ): ?>
-            <ul>
-              <?php foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
-              <li>
-                <a href="<?php echo get_permalink( $p->ID ); ?>">
-                    <?php echo get_the_title( $p->ID ); ?>
-                    <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $p->ID, "medium" ) ); ?>" alt="">
-                  </a>
-                </a>
-              </li>
-                <?php endforeach; ?>
-          </ul>
-        <?php endif; ?>
-          </div> <!-- gallery -->
+                <ul>
+                  <?php foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
+                    <li>
+                      <a href="<?php echo get_permalink( $p->ID ); ?>">
+                        <?php echo get_the_title( $p->ID ); ?>
+                        <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $p->ID, "medium" ) ); ?>" alt="">
+                      </a>
+                    </li>
+                  <?php endforeach; ?>
+                </ul>
+              <?php endif; ?>
+            </div> <!-- gallery -->
+
+          </div>
         </div>
       </div>
-    </div>
     </article>
-  <?php endwhile; else: // If 404 page error ?>
-    <article>
-
-    </article>
-  <?php endif; ?>
+  <?php endwhile; endif; ?>
 <?php get_footer(); ?>
