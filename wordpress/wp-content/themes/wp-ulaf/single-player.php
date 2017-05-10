563c3c73ce240f85b4cb6644f40df723ceb080b8
Misha Husak <i-xperia@bigmir.net>
2017-05-10 19:27:11 +0300

    single-player prettify

--- wordpress/wp-content/themes/wp-ulaf/single-player.php
+++ wordpress/wp-content/themes/wp-ulaf/single-player.php
@@ -3,16 +3,8 @@
 <section class="container container-content">
   <div class="row">
     <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
       <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-12'); ?>>
-
-
-
-
-
-
         <div class="row player-card">
-
           <div class="col-md-3 player-picture">
             <div class="player-pic">
                 <?php if ( has_post_thumbnail()) :?>
@@ -36,46 +28,37 @@
               <div class="yards">Ярды : <?php the_field('yards');?></div>
             </div>
             <div class="biography">
-              <div class="born">Родился: <span><?php
-                  // get raw date
-                  $date = get_field('player_age', false, false);
-                  // make date object
-                  $date = new DateTime($date); ?>
-                  <?php
-                  ?>
+              <div class="born">Родился:
+                  <span><?php $date = get_field('player_age', false, false); $date = new DateTime($date); ?>
+                  <?php ?>
                   <?php echo $date->format('j M Y');?> |
-                       <?php
-                        $birthday = $date->format('Y');
-                        $current_year = date("Y");
-                        $age = $current_year - $birthday;
-                        echo $age; ?></span></div>
-              <div class="nationality">Играет с: <span><?php
-                  // get raw date
-                  $date = get_field('experience', false, false);
-                  // make date object
-                  $date = new DateTime($date); ?>
-                  <?php
-                  ?>
+                  <?php $birthday = $date->format('Y'); $current_year = date("Y"); $age = $current_year - $birthday; echo $age; ?>
+                  </span>
+              </div>
+              <div class="nationality">Играет с:
+                <span>
+                  <?php $date = get_field('experience', false, false); $date = new DateTime($date); ?>
+                  <?php ?>
                   <?php echo $date->format('j M Y');?> |
-                       <?php
-                        $birthday = $date->format('Y');
-                        $current_year = date("Y");
-                        $age = $current_year - $birthday;
-                        echo $age; ?></span></div>
-              <div class="team">Игровой номер: <span><?php the_field('player_number');?></span></div>
-              <div class="height-weight">Рост и вес: <span><?php the_field('height');?> см | <?php the_field('weight');?> кг</span></div>
+                  <?php $birthday = $date->format('Y'); $current_year = date("Y"); $age = $current_year - $birthday; echo $age; ?>
+
+                </span>
+              </div>
+              <div class="team">Игровой номер:
+              <span><?php the_field('player_number');?></span>
+              </div>
+              <div class="height-weight">Рост и вес:
+                <span><?php the_field('height');?> см | <?php the_field('weight');?> кг</span>
+              </div>
               <div class="position">Позиция: <span><?php the_field('player_position');?></span></div>
             </div>
           </div>
           <div class="col-md-4 current-team">
-                    <?php
-                      $posts = get_field('player_team');
-                      if( $posts ): ?>
-                        <?php foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
-                        <a href="<?php echo get_permalink( $p->ID ); ?>" target="_blank"><?php echo get_the_post_thumbnail( $p->ID ); ?></a>
-                        <?php endforeach; ?>
-                      <?php endif; ?>
-
+            <?php $posts = get_field('player_team'); if( $posts ): ?>
+              <?php foreach( $posts as $p ): ?>
+                <a href="<?php echo get_permalink( $p->ID ); ?>" target="_blank"><?php echo get_the_post_thumbnail( $p->ID ); ?></a>
+              <?php endforeach; ?>
+            <?php endif; ?>
           </div>
         </div><!--/ player-character -->
         <h3>Offensive stats</h3>
@@ -122,15 +105,7 @@
                 <td>Lost</td>
               </tr>
               <tr class="stats">
-              <?php
-
-// check if the repeater field has rows of data
-                  if( have_rows('offensive_stats') ):
-
-  // loop through the rows of data
-                  while ( have_rows('offensive_stats') ) : the_row();?>
-
-
+              <?php if( have_rows('offensive_stats') ): while ( have_rows('offensive_stats') ) : the_row();?>
 
                 <td><?php the_sub_field('season'); ?></td>
                 <td><?php the_sub_field('oposing_team'); ?></td>
@@ -147,12 +122,6 @@
                 <td><?php the_sub_field('covered_fumbles'); ?></td>
                 <td><?php the_sub_field('losted_fumbles'); ?></td>
                  <?php endwhile; endif; ?>
-
-
-
-
-
-
               </tr>
         <!-- Конец Заголовока таблицы -->
             </table>
@@ -199,15 +168,7 @@
                 <td>Forced</td>
               </tr>
               <tr class="stats">
-                <?php
-
-// check if the repeater field has rows of data
-                  if( have_rows('defensive_stats') ):
-
-  // loop through the rows of data
-                  while ( have_rows('defensive_stats') ) : the_row();?>
-
-
+                <?php if( have_rows('defensive_stats') ): while ( have_rows('defensive_stats') ) : the_row();?>
 
                 <td><?php the_sub_field('season'); ?></td>
                 <td><?php the_sub_field('oposing_team'); ?></td>
@@ -232,45 +193,36 @@
           </div>
         </div>
         <div class="short-bio">
-
         <h3>Биография</h3>
         <p><?php the_content(); ?></p>
-
         </div>
-
         <?php comments_template(); ?>
-</article>
+      </article>
   <?php endwhile; else: ?>
   <article class="col-md-12">
-
-      <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
+    <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
   </article>
-    <?php endif; ?>
+ <?php endif; ?>
 
 
 </div><!-- /.row -->
-    <div class="row">
-    <div class="col-md-12 player-bio">
+<div class="row">
+ <div class="col-md-12 player-bio">
 
-          </div><!-- /.col-md-9 player-bio -->
-      </div><!-- /row -->
+ </div><!-- /.col-md-9 player-bio -->
+</div><!-- /row -->
 
-      <?php the_tags( __( 'Tags: ', 'wpeasy' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+<?php the_tags( __( 'Tags: ', 'wpeasy' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
 
-  <div class="owl-player-slide">
-          <?php $images = get_field('player_gallery');
-              if( $images ):
-        foreach( $images as $image ): ?>
-        <div class="item-slide">
-             <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
-        </div>
-            <?php endforeach; ?>
-            <?php endif; ?>
-  </div>
-
-
-
-  </div>
+<div class="owl-player-slide">
+  <?php $images = get_field('player_gallery'); if( $images ): foreach( $images as $image ): ?>
+    <div class="item-slide">
+      <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+    </div>
+  <?php endforeach; ?>
+ <?php endif; ?>
+</div>
+</div>
 </section>
 
 <?php get_footer(); ?>
